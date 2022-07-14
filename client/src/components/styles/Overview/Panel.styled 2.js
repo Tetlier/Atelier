53cb65a8ff886860f6260b0bea819acdf51a3d00
Jessica Chen <jessicachen012@gmail.com>
2022-07-13 17:18:10 -0700

    deleted duplicate files

--- client/src/components/styles/Overview/Panel.styled 2.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Panel = styled.div`
-  max-width: 100%;
-  margin: 0 auto;
-  padding: 15px;
-  display: flex
-`;
--- client/src/components/styles/Overview/ProductContent.styled 2.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const ProductContent = styled.div`
-  width: 35%;
-  margin-top: 60 px;
-`;
