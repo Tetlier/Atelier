6d1a46de0266c8b9f1ddbbed51f8bbbd3c3a09d2
Jessica Chen <jessicachen012@gmail.com>
2022-07-19 15:11:52 -0700

    moved cookie to global index scope

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,4 +4,16 @@ import App from './components/App.jsx';
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(<App />);
+let sessionCookie = {
+  's_id': document.cookie.slice(5),
+  actions: []
+};
+
+// takes in id (question or answer) and action (helpful or reported) to add to cookie
+const addToCookie = function(id, action) {
+  action = action.toLowerCase();
+  // adds id number + h or r for helpful or reported
+  sessionCookie.actions.push(id + action.slice(0, 1));
+};
+
+root.render(<App sessionCookie={sessionCookie} addToCookie={addToCookie}/>);
