81e742e491644da5a2eed9681312759bb6e825b2
Jesse312 <jesse.fuu@gmail.com>
2022-07-19 21:54:35 -0700

    Save changes prior to merge

--- spec/unit tests/Reviews widget/Reviews.test.js
+++ spec/unit tests/Reviews widget/Reviews.test.js
@@ -1,33 +1,38 @@
 /** @jest-environment jsdom */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Reviews from '../../client/src/components/Reviews/Reviews.jsx';
+import Reviews from '../../../client/src/components/Reviews/Reviews.jsx';
 import '@testing-library/jest-dom';
-
-
-
+import {act} from 'react-dom/test-utils';
 import renderer from 'react-test-renderer';
-import { render, screen, waitFor, cleanup, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen, cleanup, } from '@testing-library/react';
+import { toBeInTheDocument, toHaveStyle } from '@testing-library/jest-dom';
+import exampleData from './exampleData.js';
+import axios from 'axios';
 
-//cleans up residue after each test so it doesn't interfere with others
-beforeEach(()=> render(<Reviews currentProductId = '40344' currentProductRating = '3'/>))
-afterEach(() => cleanup);
 
-//test that reviews loads
-test('renders onto the DOM without crashing', () => {
-  const root = document.createElement('div');
-  ReactDOM.render(<Reviews />, root);
+jest.mock('axios');
+
+beforeEach(() => {
+  jest.clearAllMocks();
 });
 
-test('opens the form on addReview click', ()=> {
-  const addReview = screen.queryByTestId('addReview');
-  fireEvent.click(addReview);
-  const form = screen.queryByTestId('form');
-  expect(form).toBeVisible();
+describe('Reviews Widget', () => {
+  it('should render the Reviews component', async() => {
+    axios.get.mockResolvedValue(exampleData.getProducts);
+    await act(async() => {
+      render(<Reviews currentProductId={40344} currentProductRating={3.5} productName={'Camo Onesie'}/>);
+    });
+    const listElement = screen.getByText('Average Review: 3.5');
+    expect(listElement).toBeInTheDocument();
+  });
+
+  it('should log error if axios request returns an error', async() => {
+    const logSpy = jest.spyOn(console, 'log');
+    axios.get.mockResolvedValueOnce({});
+    await act(async() => {
+      render(<Reviews currentProductId={productId} currentProductRating={3.5} productName={'Camo Onesie'}/>);
+    });
+    expect(logSpy).toHaveBeenCalledWith('Error: ', expect.anything());
+  });
 });
-
-
-
-//see if things mounted on the dom correctly
-//puppetteer- clicks onto things
